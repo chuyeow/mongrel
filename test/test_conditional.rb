0d60ab2f9ede6d0d64e71adb111a5cbac0baca21
Cheah Chu Yeow <chuyeow@gmail.com>
2008-11-17 15:14:54 +0800

    Don't generate ETags.

--- test/test_conditional.rb
+++ test/test_conditional.rb
@@ -16,10 +16,10 @@ class ConditionalResponseTest < Test::Unit::TestCase
     
     @http = Net::HTTP.new(@server.host, @server.port)
 
-    # get the ETag and Last-Modified headers
+    # get the Last-Modified header
     @path = '/README'
     res = @http.start { |http| http.get(@path) }
-    assert_not_nil @etag = res['ETag']
+    assert_nil res['ETag']
     assert_not_nil @last_modified = res['Last-Modified']
     assert_not_nil @content_length = res['Content-Length']
   end
@@ -28,20 +28,9 @@ class ConditionalResponseTest < Test::Unit::TestCase
     @server.stop(true)
   end
 
-  # status should be 304 Not Modified when If-None-Match is the matching ETag
-  def test_not_modified_via_if_none_match
-    assert_status_for_get_and_head Net::HTTPNotModified, 'If-None-Match' => @etag
-  end
-
   # status should be 304 Not Modified when If-Modified-Since is the matching Last-Modified date
   def test_not_modified_via_if_modified_since
     assert_status_for_get_and_head Net::HTTPNotModified, 'If-Modified-Since' => @last_modified
-  end
-
-  # status should be 304 Not Modified when If-None-Match is the matching ETag
-  # and If-Modified-Since is the matching Last-Modified date
-  def test_not_modified_via_if_none_match_and_if_modified_since
-    assert_status_for_get_and_head Net::HTTPNotModified, 'If-None-Match' => @etag, 'If-Modified-Since' => @last_modified
   end
 
   # status should be 200 OK when If-None-Match is invalid
@@ -53,7 +42,6 @@ class ConditionalResponseTest < Test::Unit::TestCase
   # status should be 200 OK when If-Modified-Since is invalid
   def test_invalid_if_modified_since
     assert_status_for_get_and_head Net::HTTPOK,                           'If-Modified-Since' => 'invalid'
-    assert_status_for_get_and_head Net::HTTPOK, 'If-None-Match' => @etag, 'If-Modified-Since' => 'invalid'
   end
 
   # status should be 304 Not Modified when If-Modified-Since is greater than the Last-Modified header, but less than the system time
@@ -61,21 +49,18 @@ class ConditionalResponseTest < Test::Unit::TestCase
     sleep 2
     last_modified_plus_1 = (Time.httpdate(@last_modified) + 1).httpdate
     assert_status_for_get_and_head Net::HTTPNotModified,                           'If-Modified-Since' => last_modified_plus_1
-    assert_status_for_get_and_head Net::HTTPNotModified, 'If-None-Match' => @etag, 'If-Modified-Since' => last_modified_plus_1
   end
 
   # status should be 200 OK when If-Modified-Since is less than the Last-Modified header
   def test_if_modified_since_less_than_last_modified
     last_modified_minus_1 = (Time.httpdate(@last_modified) - 1).httpdate
     assert_status_for_get_and_head Net::HTTPOK,                           'If-Modified-Since' => last_modified_minus_1
-    assert_status_for_get_and_head Net::HTTPOK, 'If-None-Match' => @etag, 'If-Modified-Since' => last_modified_minus_1
   end
 
   # status should be 200 OK when If-Modified-Since is a date in the future
   def test_future_if_modified_since
     the_future = Time.at(2**31-1).httpdate
     assert_status_for_get_and_head Net::HTTPOK,                           'If-Modified-Since' => the_future
-    assert_status_for_get_and_head Net::HTTPOK, 'If-None-Match' => @etag, 'If-Modified-Since' => the_future
   end
 
   # status should be 200 OK when If-None-Match is a wildcard
@@ -91,7 +76,6 @@ class ConditionalResponseTest < Test::Unit::TestCase
       %w{ get head }.each do |method|
         res = @http.send(method, @path, headers)
         assert_kind_of response_class, res
-        assert_equal @etag, res['ETag']
         case response_class.to_s
           when 'Net::HTTPNotModified' then
             assert_nil res['Last-Modified']
